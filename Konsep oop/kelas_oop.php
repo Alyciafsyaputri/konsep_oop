73c14a3c852f15398f0179ba1ade2f1f3eee60a9
Alyciafsyaputri <smkiu.003@gmail.com>
2025-07-24 13:43:27 +0700

    belajar konsep oop

---
+++ Konsep oop/kelas_oop.php
@@ -0,0 +1,17 @@
+<?php
+
+class Product
+{
+    public $name;
+    public $price;
+    public $description;
+
+    public function __construct($name , $price , $description)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->description = $description;
+    }
+}
+
+$product = new product('Smartphone',5000000, 'Smartphone terbaru dengan kamera berkualitas tinggi');
